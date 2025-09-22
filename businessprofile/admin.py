2477442d352451765987cb85b560890d48406a04
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-09-22 17:14:10 +0600

    Business Profile

--- businessprofile/admin.py
+++ businessprofile/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import WebsiteSetting
 
 # Register your models here.
+class SiteSettingsAdmin(admin.ModelAdmin):
+    list_display = ('website_name', 'website_address', 'website_contact_phone', 'website_contact_email')
+    search_fields = ('website_name', 'website_contact_email')
+
+    def has_add_permission(self, request):
+        return not WebsiteSetting.objects.exists()
+
+admin.site.register(WebsiteSetting, SiteSettingsAdmin)
