d1a5df3cf701329c559649cf4e959655fc748188
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-09-22 08:20:32 +0600

    Register Tag & Brand

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Tag, Brand
 
-# Register your models here.
+# Register Tag
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('tag', 'tag_slug')
+    search_fields = ('tag',)
+
+admin.site.register(Tag, TagAdmin)
+
+# Register Brand
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('brand', 'brand_slug')
+    search_fields = ('brand',)
+
+admin.site.register(Brand, BrandAdmin)
