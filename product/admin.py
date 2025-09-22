55ed9f50d11b1a1fbad9d73b5cb5f6e68298527e
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-09-22 08:30:22 +0600

    Register Category

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tag, Brand
+from .models import Tag, Brand, Category
 
 # Register Tag
 class TagAdmin(admin.ModelAdmin):
@@ -14,3 +14,11 @@ class BrandAdmin(admin.ModelAdmin):
     search_fields = ('brand',)
 
 admin.site.register(Brand, BrandAdmin)
+
+# Register Category
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category', 'category_slug', 'parent_category')
+    search_fields = ('category',)
+    list_per_page = 25
+    
+admin.site.register(Category, CategoryAdmin)
