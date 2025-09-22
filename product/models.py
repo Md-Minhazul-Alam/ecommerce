d733e4c9fde690297cc494ba9a829b78eb50ed9a
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-09-22 08:22:55 +0600

    Category Add

--- product/models.py
+++ product/models.py
@@ -55,3 +55,41 @@ class Brand(models.Model):
         super().save(*args, **kwargs)
 
 
+# Category Model
+class Category(models.Model):
+    category = models.CharField(max_length=100, default='Men')
+    category_slug = models.SlugField(max_length=100, unique=True, blank=True)
+    category_image = models.ImageField(upload_to='category_images/', null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    parent_category = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='subcategories'
+    )
+
+    def __str__(self):
+        return self.category
+    
+    def save(self, *args, **kwargs):
+        if not self.category_slug:
+            base_slug = slugify(self.category)
+            slug = base_slug
+            count = 1
+            while Category.objects.filter(category_slug=slug).exists():
+                # Keep generating unique slugs
+                slug = f"{base_slug}-{count}"
+                count += 1
+            self.category_slug = slug
+        else:
+            # For manually given
+            base_slug = slugify(self.category_slug)
+            slug = base_slug
+            count = 1
+            while Category.objects.filter(category_slug=slug).exclude(pk=self.pk).exists():
+                slug = f"{base_slug}-{count}"
+                count += 1
+            self.category_slug = slug
+
+        super().save(*args, **kwargs)
