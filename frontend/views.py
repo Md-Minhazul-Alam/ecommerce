e464e61fbd9c697352fdfbc476fa25e8c922f7b3
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-09-24 18:51:04 +0600

    Slider Daynamic

--- frontend/views.py
+++ frontend/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from product.models import Category
+from themeOption.models import HeroSlider
 
 # Create your views here.
 def home_page(request):
     categoryMenu = Category.objects.all()
+    sliders = HeroSlider.objects.all()
 
-    
     return render(request, "frontend/pages/home.html", {
-        'categoryMenu': categoryMenu
+        'categoryMenu': categoryMenu,
+        'sliders': sliders,
     })
