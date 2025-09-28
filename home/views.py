d97aad87d5ccf31c297c98cd690a5e30fa7e566e
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-09-28 17:01:50 +0600

    View Fixed

--- home/views.py
+++ home/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from product.models import Category
 from themeOption.models import HeroSlider
 
-# Create your views here.
+# Home Page 
 def home_page(request):
     categoryMenu = Category.objects.all()
     sliders = HeroSlider.objects.all()
 
-    return render(request, "home/index.html", {
+    context = {
         'categoryMenu': categoryMenu,
         'sliders': sliders,
-    })
+    }
+    return render(request, "home/index.html", context)
+
