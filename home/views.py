1af0a33a9647f0b4fbcab2e450148cb4b3222f33
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-09-29 18:32:32 +0600

    Home Featured Product

--- home/views.py
+++ home/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
-from product.models import Category
+from product.models import Category, Product
 from themeOption.models import HeroSlider
+import random
 
 # Home Page 
 def home_page(request):
     categoryMenu = Category.objects.all()
     sliders = HeroSlider.objects.all()
 
+    # Featured products randomly
+    featured_products = list(Product.objects.filter(is_featured=True, is_active=True))
+    random.shuffle(featured_products)
+    featured_products = featured_products[:8]  
+
     context = {
         'categoryMenu': categoryMenu,
         'sliders': sliders,
+        'featured_products': featured_products,
     }
     return render(request, "home/index.html", context)
 
