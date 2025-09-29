691f408c7307ece23752ccfc78794c116cf47ba1
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-09-29 17:55:05 +0600

    Product Loop Add

--- product/urls.py
+++ product/urls.py
@@ -3,4 +3,5 @@ from product import views
 
 urlpatterns = [
     path('', views.all_products, name='all_products'),
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,15 +1,30 @@
-from django.shortcuts import render
-from product.models import Category
+from django.shortcuts import render, get_object_or_404
+from product.models import Category, Product
 
 
 # Home Page 
 def all_products(request):
     categoryMenu = Category.objects.all()
 
+    # Show All Products 
+    products = Product.objects.all()
+
 
     context = {
         'categoryMenu': categoryMenu,
+        'products': products,
 
     }
     return render(request, "product/products.html", context)
 
+# Product Details
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'product/product_detail.html', context)
