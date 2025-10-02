f1b49386249e3f9f5e761f7f5a5f9af151920404
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-10-02 19:28:26 +0600

    Search Function Add

--- product/views.py
+++ product/views.py
@@ -1,28 +1,31 @@
 from django.shortcuts import render, get_object_or_404
 from product.models import Category, Product
-
+from django.db.models import Q
 
 # Home Page 
 def all_products(request):
-
     # Menus
     menuCategories = Category.objects.filter(
         is_active=True,
         parent_category__isnull=True
     ).prefetch_related("subcategories")
-    
 
+    # Search query
+    query = request.GET.get("q")
 
-
-
-    # Show All Products 
-    products = Product.objects.all()
-
+    if query:
+        products = Product.objects.filter(
+            Q(product_name__icontains=query) |
+            Q(short_description__icontains=query) |
+            Q(description__icontains=query)
+        ).distinct()
+    else:
+        products = Product.objects.all()
 
     context = {
-        'menuCategories': menuCategories,
-        'products': products,
-
+        "menuCategories": menuCategories,
+        "products": products,
+        "query": query,
     }
     return render(request, "product/products.html", context)
 
