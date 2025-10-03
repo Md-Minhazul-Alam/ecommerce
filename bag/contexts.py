40de2a7cfe135c8e99dc6e15a84204be1774f707
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-10-03 17:57:23 +0600

    Cart Page Design Add

---
+++ bag/contexts.py
@@ -0,0 +1,15 @@
+# def bag_contents(request):
+
+#     bag_items = []
+#     total = 0
+#     product_count = 0
+
+
+
+
+#     if total < settings.FREE_DELIVERY_THRESHOLD:
+#         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+
+#     context = {}
+
+#     return context
