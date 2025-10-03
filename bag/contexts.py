857f5846589380ef2b0150b327fc3ec8ecae3f54
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-10-03 18:28:25 +0600

    Update Base Templete

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,15 +1,20 @@
-# def bag_contents(request):
-
-#     bag_items = []
-#     total = 0
-#     product_count = 0
+from django.conf import settings
+from decimal import Decimal
+from django.shortcuts import get_object_or_404
 
 
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    product_count = 0
 
 
-#     if total < settings.FREE_DELIVERY_THRESHOLD:
-#         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
 
-#     context = {}
 
-#     return context
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+
+    context = {}
+
+    return context
