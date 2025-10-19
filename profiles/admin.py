0e56ea4ac3922bed1a599e24f3d824b6aa6d9552
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-10-19 07:41:06 +0600

    Added User Profile Admin Customization

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone', 'city', 'country', 'created_at')
+    search_fields = ('user__username', 'phone', 'city', 'country')
+    list_filter = ('country', 'created_at')
+    ordering = ('-created_at',)
