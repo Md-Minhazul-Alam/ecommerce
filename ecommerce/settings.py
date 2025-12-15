fddcd68dc74631e65ff485ddcd6abb740d60f209
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-12-15 22:29:31 +0600

    Change STATIC_ROOT path concatenation method

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -207,7 +207,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # WhiteNoise configuration
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
