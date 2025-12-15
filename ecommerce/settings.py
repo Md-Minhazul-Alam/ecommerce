04d93b1ea35ecc0b841b38774a0ee349287f152e
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-12-15 20:19:42 +0600

    Uncomment MEDIA_URL and MEDIA_ROOT settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -222,8 +222,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
