c5a09a309dc9419b02ad6ba41a3b646dfa20c9c8
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-12-15 22:15:20 +0600

    Comment out STATICFILES_STORAGE in settings.py
    Comment out the STATICFILES_STORAGE setting for WhiteNoise.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -207,7 +207,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # WhiteNoise configuration
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Cloudinary config
 CLOUDINARY_STORAGE = {
