aeeb27c1c16bce1d899e53d5b69cbf5df5f18d72
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-12-15 23:18:42 +0600

    Change STATICFILES_STORAGE configuration
    Updated STATICFILES_STORAGE to use CompressedStaticFilesStorage.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -216,7 +216,7 @@ STATICFILES_FINDERS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # WhiteNoise configuration
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Cloudinary config
@@ -233,7 +233,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
