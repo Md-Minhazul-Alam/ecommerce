aa3eb5702ddad0285703e573ef731aa3db8a556e
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-12-15 22:32:10 +0600

    Update STATIC_URL to remove leading slash
    Remove leading slash from STATIC_URL configuration.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -203,7 +203,7 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Static files configuration
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
