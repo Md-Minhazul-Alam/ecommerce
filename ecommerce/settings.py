46dfa8c2eebeaf43324ff785d2e0677c1e7e10fc
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-12-16 16:58:07 +0600

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -49,8 +49,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'cloudinary_storage',
-    # 'cloudinary',
     'django.contrib.staticfiles',
     "whitenoise.runserver_nostatic",
     'tinymce',
@@ -194,27 +192,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-# STATIC_URL = 'static/'
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Static files configuration
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-# ]
-
-# STATICFILES_FINDERS = [
-#     'django.contrib.staticfiles.finders.FileSystemFinder',
-#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-# ]
-
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Configure Cloudinary
 cloudinary.config( 
