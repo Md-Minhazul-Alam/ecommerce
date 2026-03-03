1b00536aeb6e0bfe0102be13b6a33b404be16c20
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2026-03-03 18:48:13 +0600

    SECRET_KEY Removed & Moved heroku Env

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2swgekshzv@g%m@2hlohc&mdkou%wx9x2j8#(p+8a!kaezt)$p'
+SECRET_KEY = os.getenv('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
