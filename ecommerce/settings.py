c38e9a04a01825eefec10b174ab033588515b856
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2026-03-11 08:52:48 +0600

    Modified to solve error of ALLOWED_HOSTS

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -39,7 +39,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS]
 
 # Application definition
 
