28478f4564a8665db7c11bd702ae7ed014cc3f4a
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2026-04-01 08:20:03 +0600

    Fixed for pep8 validation of themeOption and added to TESTING.md

--- themeOption/admin.py
+++ themeOption/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import HeroSlider
 
+
 # Register your models here.
 class HeroSliderAdmin(admin.ModelAdmin):
     list_display = ('title', 'subtitle', 'image', 'link')
     search_fields = ('title', 'subtitle')
     list_filter = ('title',)
+
 
 admin.site.register(HeroSlider, HeroSliderAdmin)
